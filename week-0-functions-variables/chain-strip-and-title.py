63b03f96d853f8f34a17861e5d3c8be3ec576b26
VictorTeppeh233 <victorteppeh233@gmail.com>
2025-03-05 22:36:22 +0000

    used strip and title functions

---
+++ week-0-functions-variables/chain-strip-and-title.py
@@ -0,0 +1,8 @@
+# Ask user for name
+name = input ("Whats your name? ")
+
+# Remove whitespace from str and Capitalize user's fullname
+name = name.strip().title()
+
+# Say hello to user 
+print (f"hello, {name}")
