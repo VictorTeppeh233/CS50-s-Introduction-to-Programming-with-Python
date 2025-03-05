b1262d4a1cde68864858c4092d162599aab214bf
VictorTeppeh233 <victorteppeh233@gmail.com>
2025-03-05 22:24:05 +0000

    capitalizes the first letter of each name

---
+++ week-0-functions-variables/capitalize-the-fullname.py
@@ -0,0 +1,11 @@
+# Ask user for name
+name = input ("Whats your name? ")
+
+# Remove whitespace from str
+name = name.strip()
+
+# Capitalize the fullname
+name = name.title()
+
+# Say hello to user 
+print (f"hello, {name}")
