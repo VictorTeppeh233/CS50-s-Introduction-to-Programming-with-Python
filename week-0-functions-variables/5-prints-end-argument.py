73686408d33b2186ca345c1adebf5978ffbdcd2d
VictorTeppeh233 <victorteppeh233@gmail.com>
2025-03-10 11:20:34 +0000

    rename files

---
+++ week-0-functions-variables/5-prints-end-argument.py
@@ -0,0 +1,6 @@
+#Ask user for their name
+name = input("What's your name? ")
+
+#Using end="" prevents the print function from moving to the next line
+print("hello, ", end="")
+print(name)
--- week-0-functions-variables/6-prints-sep-argument.py
+++ week-0-functions-variables/6-prints-sep-argument.py
@@ -1,11 +1,6 @@
 #Ask user for their name
 name = input("What's your name? ")
 
-#Say hello to user
-
-#Using end="" prevents the print function from moving to the next line
-print("hello, ", end="")
-print(name)
 
 #Using sep="  " defines how much space by default should the program give when you concatenate
 print("hello,", name, sep="  ")
