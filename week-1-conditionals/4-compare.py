f3fecac169089febe0d9cb0c7ec2c6d1310d1841
VictorTeppeh233 <victorteppeh233@gmail.com>
2025-03-12 16:33:37 +0000

    using or

---
+++ week-1-conditionals/4-compare.py
@@ -0,0 +1,9 @@
+# Asks user for x and y
+x = int (input("What's x? "))
+y = int (input("What's y? "))
+
+#Using or to determine if x is equal to y or not
+if x < y or x > y:
+    print ("x is not equal to y")
+else:
+    print("x is equal to y")
