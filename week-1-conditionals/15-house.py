ecfdae4a0902ca7b641670e2ef137bc8909cf6d3
VictorTeppeh233 <victorteppeh233@gmail.com>
2025-03-12 19:53:37 +0000

    a program to tell your house at Hogwarts

---
+++ week-1-conditionals/15-house.py
@@ -0,0 +1,16 @@
+#program asks user for name
+name = input("What's your name? ")
+
+#conditional statements to tell the house at Hogwarts
+if name == "Harry":
+    print("Gryffindor")
+elif name == "Hermione":
+    print("Gryffindor")
+elif name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
+
+
