75879cc96810b7ba026c335830f6e92b2551ad63
VictorTeppeh233 <victorteppeh233@gmail.com>
2025-03-12 18:38:19 +0000

    program to print grade based on score

--- week-1-conditionals/7-grade.py
+++ week-1-conditionals/7-grade.py
@@ -1,6 +1,7 @@
+# Program asks user for score
 score = int(input("Score: "))
-
-if score >= 90 and score <=100:
+# Program displays grade based on score
+if score >= 90 and score <= 100:
     print("Grade: A")
 elif score >= 80 and score < 90:
     print("Grade: B")
