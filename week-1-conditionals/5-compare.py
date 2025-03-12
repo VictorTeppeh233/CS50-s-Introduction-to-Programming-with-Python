891ae503e90a8b44fff518bf15477fbd54f5fd2e
VictorTeppeh233 <victorteppeh233@gmail.com>
2025-03-12 16:38:10 +0000

    used !=

---
+++ week-1-conditionals/5-compare.py
@@ -0,0 +1,9 @@
+# Asks user for x and y
+x = int (input("What's x? "))
+y = int (input("What's y? "))
+
+#using != which means not
+if x != y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
