6a397085acbf12f63a158132e98f3bc0e5a6c5be
VictorTeppeh233 <victorteppeh233@gmail.com>
2025-03-12 20:00:19 +0000

    simplified the code by working on the if statement

---
+++ week-1-conditionals/16-house.py
@@ -0,0 +1,12 @@
+#program asks user for name
+name = input("What's your name? ")
+
+#conditional statements to tell the house at Hogwarts
+if name == "Harry" or name == "Hermoine" or name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
+
+
