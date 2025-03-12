d8d3ce4431ef049371c73b16a349a25a62d8f0c1
VictorTeppeh233 <victorteppeh233@gmail.com>
2025-03-12 16:08:16 +0000

    compare x and y using if statements

---
+++ week-1-conditionals/1-compare.py
@@ -0,0 +1,12 @@
+# Asks user for x and y
+x = int (input("What's x? "))
+y = int (input("What's y? "))
+
+# Compares to know if x is less, greater or equal to y.
+# Then prints the results.
+if x < y:
+    print("x is less than y")
+if x > y:
+    print("x is greater than y")
+if x == y:
+    print("x is equal to y")
