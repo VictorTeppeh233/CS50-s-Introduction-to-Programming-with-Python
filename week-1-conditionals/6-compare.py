07b19935a05f238be9782513f780f2610cc36d34
VictorTeppeh233 <victorteppeh233@gmail.com>
2025-03-12 16:47:37 +0000

    used == which means equal to

---
+++ week-1-conditionals/6-compare.py
@@ -0,0 +1,9 @@
+# Asks user for x and y
+x = int (input("What's x? "))
+y = int (input("What's y? "))
+
+#using == which means equal to 
+if x == y:
+    print("x is equal to y")
+else:
+    print("x is not equal to y")
